9acfe5561c6f18fb1ff472bec23845f3ec4f5f04
Ian S. McBride <ian.s.mcb@gmail.com>
2020-03-31 14:07:01 -0400

    Install react-bootstrap

--- WatchParty/client/src/App.js
+++ WatchParty/client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header/Header';
 import MovieList from './components/MovieList/MovieList'
