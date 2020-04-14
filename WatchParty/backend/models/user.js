9081332c4d816d9d9bb000543fd190ed60efb91f
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-14 19:49:39 -0400

    Fix code style

--- WatchParty/backend/models/user.js
+++ WatchParty/backend/models/user.js
@@ -1,10 +1,9 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING
-  }, {});
+  }, {})
   User.associate = function(models) {
     // associations can be defined here
-  };
-  return User;
-};
+  }
+  return User
+}
