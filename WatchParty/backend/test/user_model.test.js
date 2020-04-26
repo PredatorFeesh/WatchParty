fc8438aa1b9f59741aca441549353b37166803e2
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-26 16:26:49 -0400

    Add name, prop tests for User model
    - Also remove dummy test

--- WatchParty/backend/test/user_model.test.js
+++ WatchParty/backend/test/user_model.test.js
@@ -1,11 +1,23 @@
 const { expect } = require('chai')
 
-// Dummy test
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      expect([1, 2, 3].indexOf(4)).equal(-1);
-    });
-  });
-});
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists
+} = require('sequelize-test-helpers')
 
+const UserModel = require('../models/user')
+
+describe('models/user', function() {
+  const User = UserModel(sequelize, dataTypes)
+  const user = new User
+
+  checkModelName(User)('User')
+  context('properties', () => {
+    ;['email', 'firstname', 'lastname', 'password'].forEach(
+      checkPropertyExists(user)
+    )
+  })
+})
