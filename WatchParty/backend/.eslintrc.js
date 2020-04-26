f51888952070bbf50aaae0b29ec7eaf0d04542ab
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-26 14:25:34 -0400

    Add airbnb-base rules to backend linter
    - airbnb includes react specific rules
    - airbnb-base does not
    
    Mentioned here:
    - https://www.npmjs.com/package/eslint-config-airbnb
    - https://www.npmjs.com/package/eslint-config-airbnb-base

--- WatchParty/backend/.eslintrc.js
+++ WatchParty/backend/.eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true,
-        "node": true,
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    // Note: 0 ignore, 1 warn, 2 error
+    'quotes': 0,
+    'operator-linebreak': 0,
+    'arrow-parens': 1,
+    'no-use-before-define': 1,
+    'no-param-reassign': 1,
+  }
 };
