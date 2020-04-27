bb0417285207765712075979c5abbd7b5918f60d
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-27 13:08:26 -0400

    Fix lint in app.js

--- WatchParty/backend/app.js
+++ WatchParty/backend/app.js
@@ -26,7 +26,9 @@ app.use(session({
   store: new SequelizeStore({
     db: models.sequelize,
   }),
-  resave: false, // we support the touch method so per the express-session docs this should be set to false
+  // We support the touch method so per the express-session docs this
+  // should be set to false
+  resave: false,
   proxy: true, // if you do SSL outside of node.
 }));
 
