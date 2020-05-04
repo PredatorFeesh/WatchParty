36ba396eafd1af446c583aa61b983b7582ea94b6
usersblock <thomasly8991@gmail.com>
2020-05-03 21:52:17 -0400

    Revert "Fixed lint issues, deleted Follow methds, created method tests"
    This reverts commit 06f2231ed928c3fd54866e5c13bf9f21a5772690.

--- WatchParty/backend/test/follow_model.test.js
+++ WatchParty/backend/test/follow_model.test.js
@@ -1,63 +1,43 @@
-const { chai, expect } = require('chai');
-const sinon = require('sinon');
-chai.use(require('sinon-chai'));
-const expect = chai.expect;
+const chai = require('chai')
+const sinon = require('sinon')
+chai.use(require('sinon-chai'))
+const expect = chai.expect
 
 const {
   sequelize,
   dataTypes,
   checkModelName,
   checkUniqueIndex,
-  checkPropertyExists,
-} = require('sequelize-test-helpers');
+  checkPropertyExists
+} = require('sequelize-test-helpers')
 
-const FollowModel = require('../models/follow');
+const FollowModel = require('../models/follow')
 
-describe('models/follow', function () {
-  const Follow = FollowModel(sequelize, dataTypes);
-  const follow = new Follow();
+describe('models/follow', function() {
+  const Follow = FollowModel(sequelize, dataTypes)
+  const follow = new Follow
 
-  checkModelName(Follow)('Follow');
+  checkModelName(Follow)('Follow')
   context('properties', () => {
-    ['followee', 'follower'].forEach(
-      checkPropertyExists(follow),
-    );
-  });
+    ;['followee', 'follower'].forEach(
+      checkPropertyExists(follow)
+    )
+  })
   context('associations', () => {
-    const User1 = 'some dummy user-1';
-    const User2 = 'some dummy user-2';
+    const User1 = 'some dummy user-1'
+    const User2 = 'some dummy user-2'
 
     before(() => {
-      Follow.associate({ User: User1 });
-      Follow.associate({ User: User2 });
-    });
+      Follow.associate({ User: User1 })
+      Follow.associate({ User: User2 })
+    })
 
     it('defined a belongsTo association with User1', () => {
-      expect(Follow.belongsTo).to.have.been.calledWith(User1);
-    });
+      expect(Follow.belongsTo).to.have.been.calledWith(User1)
+    })
 
     it('defined a belongsTo association with User2', () => {
-      expect(Follow.belongsTo).to.have.been.calledWith(User2);
-    });
-  });
-  context('Called getFollowersOf function', () => {
-    before(async () => {
-      const fakeGetFollowersOf = sinon.fake.returns('Executed');
-      sinon.replace(Follow, 'getFollowersOf', fakeGetFollowersOf);
-      Follow.getFollowersOf(1);
-    });
-    it('getFollowersOf was called', () => {
-      expect(Follow.getFollowersOf).to.have.been.called;
-    });
-  });
-  context('Called getFolloweesOf function', () => {
-    before(async () => {
-      const fakeGetFolloweesOf = sinon.fake.returns('Executed');
-      sinon.replace(Follow, 'getFolloweesOf', fakeGetFolloweesOf);
-      Follow.getFolloweesOf(1);
-    });
-    it('getFolloweesOf was called', () => {
-      expect(Follow.getFolloweesOf).to.have.been.called;
-    });
-  });
-});
+      expect(Follow.belongsTo).to.have.been.calledWith(User2)
+    })
+  })
+})
