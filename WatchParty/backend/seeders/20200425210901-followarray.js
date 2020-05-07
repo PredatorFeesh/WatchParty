aefc112c4b489662dfe893dee97258589885c8ba
Ian S. McBride <ian.s.mcb@gmail.com>
2020-05-07 18:03:05 -0400

    Fix User-Follow association
    - Remove explicit Follow model
    - Add "belongsToMany" assocations between User and User through an
      auxillary UserFollower table
    - Update seeder for user and follower data

--- WatchParty/backend/seeders/20200425210901-followarray.js
+++ WatchParty/backend/seeders/20200425210901-followarray.js
@@ -4,21 +4,29 @@ module.exports = {
     const FollowArray = [];
     for (let i = 1; i < 10; i += 1) {
       FollowArray.push({
-        follower: i,
-        followee: 20,
+        followerId: i,
+        followeeId: 20,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
     for (let i = 1; i < 10; i += 1) {
       FollowArray.push({
-        follower: 20,
-        followee: i,
+        followerId: 20,
+        followeeId: i,
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
-    return queryInterface.bulkInsert("Follow", FollowArray);
+    for (let i = 1; i < 5; i += 1) {
+      FollowArray.push({
+        followerId: i,
+        followeeId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    return queryInterface.bulkInsert("UserFollower", FollowArray);
   },
   /* eslint-disable no-unused-vars */
   down: (queryInterface, Sequelize) => queryInterface.bulkDelete("Follow", null),
