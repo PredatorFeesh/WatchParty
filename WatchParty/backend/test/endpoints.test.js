def2f90482cd0423cf9b3c1eb87168d3b44dec58
rxkt <liao.eric@outlook.com>
2020-05-11 21:23:25 -0400

    added timeouts for mocha test, added more tests to pingpong

--- WatchParty/backend/test/endpoints.test.js
+++ WatchParty/backend/test/endpoints.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
+const bcrypt = require('bcrypt');
 const app = require('../app');
 
 const { expect } = chai;
 chai.use(chaiHttp);
 describe('Server', () => {
-  it('does not pong on wrong input', done => {
+  it('does not pong on wrong input (error 401)', done => {
     chai
       .request(app)
       .post('/api/ping')
@@ -14,6 +14,34 @@ describe('Server', () => {
       .end((err, res) => {
         expect(res).to.have.status(401);
       });
+  });
+  
+  it('results in error on no input (error 400)', done => {
+    chai
+      .request(app)
+      .post('/api/ping')
+      .send({ message: 'ping'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+      });
+  });
+  
+  it('pongs on correct input (status 200)', done => {
+	const secretWord = 'dodo';
+	const timestamp = Math.floor(Date.now() / 100000);
+    const combination = secretWord + timestamp.toString();  
+	const hashTimestamp = async () => {
+      const hash = await bcrypt.hash(combination, 1);
+      chai
+        .request(app)
+        .post('/api/ping')
+        .send({ message: 'ping', token: hash })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+        });
+    }
+	
+    
   });
   
   // add right input, and no input
