e0bb9d1849622966dcc51145f54ff3d5ba13b3b5
Jason <azayevjason@gmail.com>
2020-05-14 02:20:31 -0400

    Added index test

--- WatchParty/client/src/__test__/App.test.js
+++
@@ -1,8 +0,0 @@
-import App from '../App';
-
-test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  console.log(App); // eslint-disable-line no-console
-});
